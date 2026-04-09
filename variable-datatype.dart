c99e7ebb8e7b6c7342fe085209c08e018cbdc3c5
Ahmad-kamal1 <ahmikams123@gmail.com>
2026-04-09 14:30:10 +0500

    add new file variable and datatype.

--- variable-datatype.dart
+++ variable-datatype.dart
@@ -1,5 +1,18 @@
 void main() {
   int number1; // Declare a varaible of type int.
   number1 = 10; // initialize the varaible with a vale.
+
   print(number1);
+
+  // Add two varaibles of type int.
+  int num1; // declare first variable.
+  int num2; // declare second variable.
+  int sum; // declare third variable which store sum of that two varaibles.
+
+  num1 = 20; // initialize first variable.
+  num2 = 60; // initialize second variable.
+  sum =
+      num1 + num2; // initialize third variable with sum of first and second variables.
+
+  print(sum);    // print the sum of two variables.
 }
