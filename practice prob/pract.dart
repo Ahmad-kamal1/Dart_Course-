c5ba0f6fd8f74e195ff7a5bcfafd7e528662b0e9
Ahmad-kamal1 <ahmikams123@gmail.com>
2026-04-09 16:52:26 +0500

    practice questions.

--- practice prob/pract.dart
+++ practice prob/pract.dart
@@ -1,6 +1,5 @@
-// Write Program to Swap Values of Two Variables
-
 void main() {
+  // Write Program to Swap Values of Two Variables
   int x;
   int y;
   x = 10;
@@ -47,4 +46,32 @@ output : (a = 5 , b = 3 , c = 4) */
   print("value of a: $a");
   print("value of b: $b");
   print("value of c: $c");
+/************************************************************************* */
+
+// Write Program to convert feet to metres and metres into KM.
+  double feet;
+  double meters;
+  double km;
+
+  feet = 100;
+  meters = feet / 3;
+  km = meters / 1000;
+  print("$feet feet is equal to:");
+  print("Meters: $meters");
+  print("KM: $km");
+  /**************************************************************************/
+
+  //Write Program to convert celcius to farenheit and farenheit to celcius.
+  double cel;
+  double far;
+  cel = 35.0;
+
+  far = (cel * 9 / 5) + 32;
+  print("$cel degree celcius is equal to $far degree farenheit.");
+
+  far = 87.0;
+  cel = (far - 32) * 5 / 9;
+  print("$far degree farenheit is equal to $cel degree celcius.");
+
+
 }
