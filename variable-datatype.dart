218182aa7e5d4078d5314498dc1e33e213baa9a9
Ahmad-kamal1 <ahmikams123@gmail.com>
2026-04-09 15:16:29 +0500

    variables and datatypes.

--- variable-datatype.dart
+++ variable-datatype.dart
@@ -4,7 +4,7 @@ void main() {
 
   print(number1);
 
-  // Add two varaibles of type int.
+  // perform arthematic operation on variables.
   int num1; // declare first variable.
   int num2; // declare second variable.
   int sum; // declare third variable which store sum of that two varaibles.
@@ -12,7 +12,27 @@ void main() {
   num1 = 20; // initialize first variable.
   num2 = 60; // initialize second variable.
   sum =
-      num1 + num2; // initialize third variable with sum of first and second variables.
+      num1 +
+      num2; // initialize third variable with sum of first and second variables.
 
-  print(sum);    // print the sum of two variables.
+  print("Sum = $sum"); // print the sum of two variables.
+  //----------------------------------------------------------------------------
+  // subtraction of intergers.
+  int sub = num1 - num2;
+  print("Subtraction = $sub"); // subtration of ywo integers.
+  //---------------------------------------------------------------------------
+  //multiplication of intergers.
+
+  int mul = num1 * num2;
+  print("Multiplication = $mul"); //multiplication of two integers.
+
+  //---------------------------------------------------------------------------
+
+  //division of intergers.
+  double div = num2 / num1;
+  print("Division = $div"); // division of two integers.
+  //--------------------------------------------------------------------------
+  // modulus of integers.
+  int mod = num2 % num1;
+  print("Modulus = $mod");  // modulus of two integers.
 }
