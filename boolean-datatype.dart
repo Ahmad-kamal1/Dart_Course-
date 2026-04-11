b109bb6dff5389441f39e472825ff6a97e1a2195
Ahmad-kamal1 <ahmikams123@gmail.com>
2026-04-11 13:55:43 +0500

    Boolean datatype in Dart.

---
+++ boolean-datatype.dart
@@ -0,0 +1,12 @@
+// Boolean datatype in Dart.
+
+void main() {
+  int num1 = 10; // first variable.
+  int num2 = 30; // second variable.
+  int sum = num1 + num2; // sum of first and second variable.
+  bool result =
+      sum >
+      50; // boolean datatype to check if sum is greater than 50 it will return true or false.
+
+  print(result);
+}
