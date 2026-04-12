c0f16681fe6b35d85d8caee478dd4bed39a07554
Ahmad-kamal1 <ahmikams123@gmail.com>
2026-04-12 11:41:01 +0500

    Relational and Equality operator in Dart.

--- relational-and-equality-operator.dart
+++ relational-and-equality-operator.dart
@@ -3,3 +3,13 @@ import 'dart:io';
 // Relational and Equality operator in Dart.
 // Relational operators: <, >, <=, >=.
 // Equality operators: ==, !=.
+void main() {
+  print("Hello and welcome to Dart course");
+
+  stdout.write("Enter value of X: ");
+  int x = int.parse(stdin.readLineSync()!); // Tale value of x from user.
+  stdout.write("Enter value of Y: ");
+  int y = int.parse(stdin.readLineSync()!); // Take value of y from user.
+
+  
+}
