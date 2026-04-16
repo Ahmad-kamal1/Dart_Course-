c8b4baa91e22386c3028863cfc4959694338652e
Ahmad-kamal1 <ahmikams123@gmail.com>
2026-04-16 10:44:10 +0500

    Logical operator in Dart.

--- logical-operator.dart
+++ logical-operator.dart
@@ -1,5 +1,24 @@
 import 'dart:io';
+
 // Logical operators in Dart:
 // And operator (&&).
 // OR operator (||).
 // NOT operator (!).
+void main() {
+  print(" Logical operators in Dart.");
+
+  stdout.write("Enter value of a: ");
+  int a = int.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter value of b: ");
+  int b = int.parse(stdin.readLineSync()!);
+  /*  AND operator (&&):
+ 
+  true + true = true
+  true + false = false
+  false + true = false
+  false + false = false
+*/
+  bool result = (a > b) && (b < a);
+  print("Result of AND operator (&&): $result");
+}
